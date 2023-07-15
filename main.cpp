ea51c3bdeb7a32be98f53989ab6148bce401234b
Kamil <kamilratajczyk00@gmail.com>
2023-07-15 16:53:32 +0200

    Add sprite object

--- main.cpp
+++ main.cpp
@@ -2,9 +2,29 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
+class Passenger{
+    private:
+        int startLevel;
+        int endLevel;
+        int mass = 70;
+
+    public:
+
+};
+
 int main(){
     sf::RenderWindow window(sf::VideoMode(1200, 700), "Elevator Simulation");
+
     sf::Texture texture;
+    if (!texture.loadFromFile("img/person_sprite.jpg", sf::IntRect(0, 0, 800, 2000)))
+    {
+        std::cout << "Error " << std::endl;
+    }
+    texture.setSmooth(true);
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);   
+// sa
 
     window.setFramerateLimit(60);
     while(window.isOpen()){
@@ -20,6 +40,7 @@ int main(){
         }
 
         window.clear(sf::Color(255,255,255));
+        window.draw(sprite);
         window.display();
     }
     return 0;
