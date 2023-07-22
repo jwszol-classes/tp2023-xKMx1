85a8ea47ac9cc430c6a8ca24a4c0916bf2651d2b
KOZAKtotalny <pituchamaciej10@gmail.com>
2023-07-22 18:59:12 +0200

    dodałem jeszcze te ściany ale mozesz ogarnąc zeby byly w jednej klasie i nwm jak zrobic zeby byly cieńsze

--- main.cpp
+++ main.cpp
@@ -7,20 +7,49 @@ private:
     int mPassengerMass;
     int mCurrentLevel;
     sf::RectangleShape mRectangle;
+    sf::RectangleShape mLine;
+    sf::RectangleShape mLine2;
+
+
 public:
     Elevator() {
+        //rectangle
         sf::Vector2f sizeOfRectangle(100.0f, 100.0f);
         mRectangle.setPosition(300.0f, 100.0f);
         mRectangle.setOutlineColor(sf::Color::Black);
         mRectangle.setSize(sizeOfRectangle);
         mRectangle.setFillColor(sf::Color::White);
         mRectangle.setOutlineThickness(10);
+        //lines next to elevator(walls)
+        sf::Vector2f sizeOfLine(400.0f, 0.0f);
+        mLine.setSize(sizeOfLine);
+        mLine.setPosition(450.0f, 100.0f);
+        mLine.setOutlineColor(sf::Color::Black);
+        mLine.setFillColor(sf::Color::Black);
+        mLine.setOutlineThickness(10);
+        mLine.rotate(90);
+
+        mLine2.setSize(sizeOfLine);
+        mLine2.setPosition(250.0f, 100.0f);
+        mLine2.setOutlineColor(sf::Color::Black);
+        mLine2.setFillColor(sf::Color::Black);
+        mLine2.setOutlineThickness(10);
+        mLine2.rotate(90);
+        
         
     }
     sf::RectangleShape get_rectangle() {
         return mRectangle;
+        
 
     }
+    sf::RectangleShape get_line() {
+        return mLine;
+    }
+    sf::RectangleShape get_line2() {
+        return mLine2;
+    }
+    
 };
 
 class Passenger {
@@ -62,6 +91,9 @@ int main() {
     Passenger first_passenger(&texture);
     Elevator main_elevator;
     
+
+
+    
     window.setFramerateLimit(60);
     while (window.isOpen()) {
         sf::Event event;
@@ -78,6 +110,9 @@ int main() {
         window.clear(sf::Color(255, 255, 255));
         window.draw(first_passenger.getSprite());
         window.draw(main_elevator.get_rectangle());
+        window.draw(main_elevator.get_line());
+        window.draw(main_elevator.get_line2());
+
         window.display();
     }
     return 0;
