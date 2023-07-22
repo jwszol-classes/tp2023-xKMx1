47d0a615886aae53a89143331e7fd45408570891
KOZAKtotalny <pituchamaciej10@gmail.com>
2023-07-22 11:34:52 +0200

    ko11m

--- main.cpp
+++ main.cpp
@@ -2,14 +2,30 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
-class Passenger{
-    private:
-        int startLevel;
-        int endLevel;
-        int mass = 70;
+class Passenger {
+private:
+    int mStartLevel;
+    int mEndLevel;
+    int mMass = 70;
+    sf::Sprite mSprite;
+public:
+    Passenger(const sf::Texture* texture) {
+        mSprite.setTexture(*texture);
+        mSprite.setScale(0.25f, 0.25f);
+        mSprite.setPosition(100.0f, 100.0f);
+    }
 
-    public:
+    int getStartLevel() {
+        return mStartLevel;
+    }
 
+    int getEndLevel() {
+        return mEndLevel;
+    }
+
+    sf::Sprite getSprite() {
+        return mSprite;
+    }
 };
 
 int main() {
@@ -37,8 +53,8 @@ int main() {
             }
         }
 
-        window.clear(sf::Color(255,255,255));
-        window.draw(sprite);
+        window.clear(sf::Color(255, 255, 255));
+        window.draw(first_passenger.getSprite());
         window.display();
     }
     return 0;
